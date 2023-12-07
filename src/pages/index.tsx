b2750d67ebae1e85263423d2fd69ce2109c57b4d
Majid Hajian <mhadaily@gmail.com>
2023-12-07 11:03:16 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,7 @@ export default () => {
     <NiceModal.Provider>
       <Layout>
         <SEO />
-        <center>
-          <h3>With heavy hearts, we announce FlutterVikings 2024 will not happen. Scheduling conflicts with other great community events have made it impossible to host the conference again. We're incredibly grateful for the amazing community of Flutter developers who have supported us💙</h3>
-        </center>
-        {/* <Schedules /> */}
+         <Schedules /> 
         {/* <Scene /> */}
         {/* <Sponsors /> */}
         {/* <Speakers /> */}
